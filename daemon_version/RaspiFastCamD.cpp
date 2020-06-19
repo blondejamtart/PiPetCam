269b7836295313e11a3578edaf117c578c8695a7
Bryan T <blondejamtart@gmail.com>
2020-06-19 11:19:46 +0100

    fixed msg size on send

--- daemon_version/RaspiFastCamD.cpp
+++ daemon_version/RaspiFastCamD.cpp
@@ -308,7 +308,7 @@ void image_to_zmq(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer, void *userDat
             /* Send the message to the socket */
             if (pData->total_bytes > 0 && pData->data_message)
             {
-                rc |= (zmq_send(pData->socket, pData->data_message, pData->sent_bytes, 0) == 0);
+                rc |= (zmq_send(pData->socket, pData->data_message, pData->total_bytes, 0) == 0);
                 printf("Image sent\n");
             }
             complete = 1;
